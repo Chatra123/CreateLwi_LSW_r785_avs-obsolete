40887d683d80d1ce306cd2c3a8854d11304c3432
a <a>
2015-08-10 21:54:02 +0900

    build plugin

--- AviSynth/lsw-build1__LSMASHSourceVCX.vcxproj.bat
+++
@@ -1,4 +0,0 @@
-set CL=/I..\..\..\..\..\include /I..\..\msinttypes
-set LINK="libpthread.a" "libiconv.a" "libswresample.a" "libmsvcrt.a" /LIBPATH:..\..\..\..\..\i686-w64-mingw32\lib /LIBPATH:..\..\..\..\..\lib\gcc\i686-w64-mingw32\4.8.2 /LIBPATH:..\..\bzip2 /LIBPATH:..\..\..\..\..\lib
-@for /d %%1 in (%SystemRoot%\Microsoft.NET\Framework\v*) do @if exist "%%~1\msbuild.exe" set "MSBUILD=%%~1\msbuild.exe"
-"%MSBUILD%" LSMASHSourceVCX.vcxproj /target:Rebuild /property:Configuration=Release;Platform=Win32;PlatformToolset=v140 /toolsversion:14.0
